ef491cadd1422effff6ab05e4afdf9cb3a4f993f
kukaan <58886775+kukaan@users.noreply.github.com>
2020-11-18 13:39:37 +0200

    first test. gradle build and gradle test work

---
+++ src/test/java/kapistelykirjasto/ApplicationTest.java
@@ -0,0 +1,14 @@
+package kapistelykirjasto;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Before
+    public void setUp(){
+    }
+
+    @Test
+    public void hello() {
+    }
+}
