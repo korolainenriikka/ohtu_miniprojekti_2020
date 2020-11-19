ed0399a344882abcce4dae0175f3d03ee2d32206
johannaval <johanna.valminen@gmail.com>
2020-11-19 17:01:38 +0200

    fixed StubDao

--- src/main/java/kapistelykirjasto/dao/StubDao.java
+++ src/main/java/kapistelykirjasto/dao/StubDao.java
@@ -5,23 +5,37 @@ import java.util.ArrayList;
 public class StubDao implements Dao {
 
     private ArrayList<Entry> entryes = new ArrayList<>();
+    private boolean closed = false;
 
     @Override
     public boolean createEntry(Entry entry) {
 
-        if (entry.getTitle().equals(entry.getTitle())) {
+        if (!closed) {
+            entryes.add(entry);
+            return true;
+
+        } else {
             return false;
+        }
+    }
+
+    @Override
+    public boolean sameTitleAlreadyExists(String title) {
+
+        if (!closed) {
+
+            if (entryes.equals(title)) {
+                return false;
+            } else {
+                return true;
+            }
         }
         return true;
     }
 
     @Override
-    public boolean sameTitleAlreadyExists(String title) {
-        return false;
-    }
-
-    @Override
     public void close() {
 
+        this.closed = true;
     }
 }
